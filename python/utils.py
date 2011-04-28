aa6fca897bcb6335179e85be9b04e648fc1d0945
Bruno Turcksin <bruno.turcksin@gmail.com>
2011-04-28 11:32:00 -0500

    Utils is not a class anymore, just a module.

--- python/utils.py
+++ python/utils.py
@@ -8,55 +8,51 @@
 
 """Module containing some miscellenous function."""
 
-class utils  :
-    """Class used to help to manipulate input and output files and errors. """
-
-
-    def abort(self,message) :
-        print (message)
-        print ("The program aborted.")
-        exit()
+def abort(message) :
+  print (message)
+  print ("The program aborted.")
+  exit()
 
 #----------------------------------------------------------------------------#
 
-    def search_in_line(self,line,keyword) :
-        """Search for the given keyword in a given line. If the keyword is
-        found, return True else return False."""
+def search_in_line(line,keyword) :
+  """Search for the given keyword in a given line. If the keyword is
+  found, return True else return False."""
 
-        found = False;
-        position = line.find(keyword)
-        if position != -1 :
-            found = True
+  found = False;
+  position = line.find(keyword)
+  if position != -1 :
+      found = True
 
-        return found
+  return found
 
 #----------------------------------------------------------------------------#
 
-    def read_float(self,line,n_floats) :
-        """Read the first n_floats in the given line. The floats have to be 
-        separate by blanks. The values are returned in a list."""
+def read_float(line,n_floats) :
+  """Read the first n_floats in the given line. The floats have to be 
+  separate by blanks. The values are returned in a list."""
 
-        vector = []
-        pos = 0
-        read = True
-        for i in xrange(0,n_floats) :
+  vector = []
+  pos = 0
+  read = True
+  for i in xrange(0,n_floats) :
 # We read the next element
-            value = ' '
+      value = ' '
 # We skip the blanks
-            while line[pos] == " " :
-                pos += 1
+      while line[pos] == " " :
+          pos += 1
 # We read the value
-            while line[pos] != " " :
-                if line[pos] != "\n" :
-                    value += line[pos]
-                    pos += 1
-                else :
-                    break
-            if value != " " :
-                value = float(value)
-            else :
-                read = False
-                break
-            vector.append(value)
+      while line[pos] != " " :
+          if line[pos] != "\n" :
+              value += line[pos]
+              pos += 1
+          else :
+              break
+      if value != " " :
+          value = float(value)
+      else :
+          read = False
+          break
+      vector.append(value)
 
-        return vector,read
+  return vector,read
