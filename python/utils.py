ca6c8c7aadda4ec56275e6babdf1fcef0422bba0
Bruno Turcksin <bruno.turcksin@gmail.com>
2012-03-17 13:55:01 -0500

    Change the first letter of each function with an upper case letter.

--- python/utils.py
+++ python/utils.py
@@ -3,19 +3,22 @@
 # Date: 2010-10-08 15:51:19.904088
 
 #----------------------------------------------------------------------------#
-## Class utils                                                              ##
+## Module utils                                                              ##
 #----------------------------------------------------------------------------#
+
+import numpy as np
+import matplotlib.pyplot
 
 """Module containing some miscellenous function."""
 
-def abort(message) :
+def Abort(message) :
   print (message)
   print ("The program aborted.")
   exit()
 
 #----------------------------------------------------------------------------#
 
-def search_in_line(line,keyword) :
+def Search_in_line(line,keyword) :
   """Search for the given keyword in a given line. If the keyword is
   found, return True else return False."""
 
@@ -28,7 +31,7 @@ def search_in_line(line,keyword) :
 
 #----------------------------------------------------------------------------#
 
-def read_float(line,n_floats) :
+def Read_float(line,n_floats) :
   """Read the first n_floats in the given line. The floats have to be 
   separate by blanks. The values are returned in a list."""
 
@@ -56,3 +59,21 @@ def read_float(line,n_floats) :
       vector.append(value)
 
   return vector,read
+
+#----------------------------------------------------------------------------#
+
+def Plot_pattern(matrix) :
+  """Plot the pattern of the given matrix, i.e. plot the non-zeros elements of
+  the matrix."""
+
+  x = []
+  y = []
+  for j in xrange(0,matrix.shape[1]) :
+    for i in xrange(0,matrix.shape[0]) :
+      if np.abs(matrix[i,j]) > 1e-12 :
+        x.append(matrix.shape[0]-i)
+        y.append(j+1)
+  matplotlib.pyplot.plot(y,x,'x')
+  matplotlib.pyplot.xlim(0,matrix.shape[1]+1)
+  matplotlib.pyplot.ylim(0,matrix.shape[0]+1)
+  matplotlib.pyplot.show()
