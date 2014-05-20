b705e9595971cc304d0f6bba19613d9bce2f0e9b
Bruno Turcksin <bruno.turcksin@gmail.com>
2014-05-20 10:47:47 -0500

    Add bit interleaving for 2D domain.

--- morton_curve/morton_curve.cc
+++ morton_curve/morton_curve.cc
@@ -4,7 +4,7 @@
  * For 128 bits, there is not built-in type in C++ so this function need to be
  * commented.
  *
- * uint128_t split_by_3bits(uint128_t u)
+ * uint128_t separate_by_2(uint128_t u)
  * {
  *   // Keep the 42 least-signigicant bits
  *   u &= 0x3ffffffffff;
@@ -20,11 +20,12 @@
  * }
  */
 
-unsigned long long int split_by_3bits(unsigned long long int u)
+unsigned long long int separate_by_2(unsigned long long int u)
 {
   // Keep the 21 least-significant bits
   u &= 0x1fffff;
 
+  // Shift the bits and filter them
   u = (u | u<<32) & 0x1f00000000ffff;
   u = (u | u<<16) & 0x1f0000ff0000ff;
   u = (u | u<<8) & 0x100f00f00f00f00f;
@@ -34,27 +35,63 @@ unsigned long long int split_by_3bits(unsigned long long int u)
   return u;
 }
 
+unsigned long long int separate_by_1(unsigned long long int u)
+{
+  // Keep the 32 least-significant bits
+  u &= 0xffffffff;
+
+  // Shift the bits and filter them
+  u = (u | u<<16) & 0x0000ffff0000ffff;
+  u = (u | u<<8) & 0x00ff00ff00ff00ff;
+  u = (u | u<<4) & 0x0f0f0f0f0f0f0f0f;
+  u = (u | u<<2) & 0x3333333333333333;
+  u = (u | u<<1) & 0x5555555555555555;
+
+  return u;
+}
 
 // Return an unsigned long long int but only using 63 bits. The inputs are also unsigned
 // long long int but we use only 21 bits.
 unsigned long long int compute_morton_ordering(unsigned long long int x, 
     unsigned long long int y, unsigned long long int z)
 {
-  unsigned long long int splitted_x = split_by_3bits(x);
-  unsigned long long int splitted_y = split_by_3bits(y);
-  unsigned long long int splitted_z = split_by_3bits(z);
+  unsigned long long int splitted_x = separate_by_2(x);
+  unsigned long long int splitted_y = separate_by_2(y);
+  unsigned long long int splitted_z = separate_by_2(z);
 
   // Return the order
   return splitted_x | (splitted_y<<1) | (splitted_z<<2);
 }
 
+// Return an unsigned long long int. The inputs are also unsigned long long int
+// but we only use 32 bits.
+unsigned long long int compute_morton_ordering(unsigned long long int x,
+    unsigned long long int y)
+{
+  unsigned long long int splitted_x = separate_by_1(x);
+  unsigned long long int splitted_y = separate_by_1(y);
+
+  // Return the order
+  return splitted_x | (splitted_y<<1);
+}
+
 int main()
 {
-  unsigned long long x(5); 
-  unsigned long long y(9); 
-  unsigned long long z(1); 
+  unsigned long long int x1(5); 
+  unsigned long long int y1(9); 
+  unsigned long long int z1(1); 
+
+  unsigned long long int x2(400);
+  unsigned long long int y2(300);
+  unsigned long long int z2(12);
+
+  unsigned long long int x3(800);
+  unsigned long long int y3(600);
   
-  std::cout<<compute_morton_ordering(x,y,z)<<std::endl;
+  std::cout<<compute_morton_ordering(x1,y1,z1)<<" = "<<1095<<std::endl;
+  std::cout<<compute_morton_ordering(x2,y2,z2)<<" = "<<52501888<<std::endl;
+
+  std::cout<<compute_morton_ordering(x3,y3)<<" = "<<861824<<std::endl;
 
   return 0;
 }
